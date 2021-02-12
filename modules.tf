ab75ff778401c764e636a129569094778cc68b93
Ronald Konjer <ronaldkonjer@gmail.com>
2021-02-12 11:42:05 +0100

    tf 0.14upgrade

--- modules.tf
+++ modules.tf
@@ -5,7 +5,7 @@
 ##############################################################################
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.24.1"
   name       = var.name
   namespace  = var.namespace
   stage      = var.environment
